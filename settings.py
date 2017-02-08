dc49e07435c2b178e09c102cf63108d89283e896
ape364 <ape364@gmail.com>
2017-02-09 01:11:54 +0500

    shelve database path fixed

--- settings.py
+++ settings.py
@@ -8,4 +8,4 @@ FILESIZE_LIMIT_MIN_IN_BYTES = 16
 FILESIZE_LIMIT_MAX_IN_BYTES = 1024 * 16
 
 GITHUB_API_URL = 'https://api.github.com/gists'
-SHELVE_FILENAME = 'shelve.db'
+SHELVE_FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'shelve.db')
