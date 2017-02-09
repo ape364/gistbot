a170164b810b05bd354881552dd17a4a1761c59f
ape364 <ape364@gmail.com>
2017-02-09 22:14:29 +0500

    using .get() to avoid of KeyError on localhost

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ FILESIZE_LIMIT_MAX_IN_BYTES = 1024 * 16
 GITHUB_API_URL = 'https://api.github.com/gists'
 SHELVE_FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'shelve.db')
 
-REPO_DIR = os.environ['OPENSHIFT_REPO_DIR']
-DOMAIN_NAME = os.environ['OPENSHIFT_GEAR_DNS']
-SERVER_IP = os.environ['OPENSHIFT_PYTHON_IP']
-SERVER_PORT = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+REPO_DIR = os.environ.get('OPENSHIFT_REPO_DIR')
+DOMAIN_NAME = os.environ.get('OPENSHIFT_GEAR_DNS')
+SERVER_IP = os.environ.get('OPENSHIFT_PYTHON_IP')
+SERVER_PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT'))
